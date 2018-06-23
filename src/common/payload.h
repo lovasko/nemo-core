be7e832412c85078595bb195f64ef7bdfc649994
Daniel Lovasko <daniel.lovasko@gmail.com>
2018-06-23 14:19:48 +0100

    Initial datagram payload definition.

---
+++ src/common/payload.h
@@ -0,0 +1,43 @@
+// Copyright (c) 2018 Daniel Lovasko.
+// All Rights Reserved
+//
+// Distributed under the terms of the 2-clause BSD License. The full
+// license is in the file LICENSE, distributed as part of this software.
+
+#ifndef NEMO_COMMON_PAYLOAD_H
+#define NEMO_COMMON_PAYLOAD_H
+
+#include <stdint.h>
+
+
+/// Message types.
+#define NEMO_PAYLOAD_TYPE_RESPONSE 1
+#define NEMO_PAYLOAD_TYPE_REQUEST  2
+
+/// Constants.
+#define NEMO_PAYLOAD_MAGIC 0x6e656d6f
+#define NEMO_PAYLOAD_VERSION        1
+
+
+// Diagnostic payload.
+typedef struct _payload {
+  uint32_t pl_mgic;   ///< Magic identifier.
+  uint8_t  pl_fver;   ///< Format version.
+  uint8_t  pl_type;   ///< Message type.
+  uint16_t pl_port;   ///< UDP port.
+  uint8_t  pl_ttl1;   ///< Time-To-Live in requester.
+  uint8_t  pl_ttl2;   ///< Time-To-Live in responder.
+  uint8_t  pl_pad[6]; ///< Padding (unused).
+  uint64_t pl_snum;   ///< Sequence iteration number.
+  uint64_t pl_slen;   ///< Sequence length.
+  uint64_t pl_laddr;  ///< IP address - low-bits.
+  uint64_t pl_haddr;  ///< IP address - high-bits.
+  uint64_t pl_reqk;   ///< Requester unique key.
+  uint64_t pl_resk;   ///< Responder unique key.
+  uint64_t pl_mtm1;   ///< Steady time of request.
+  uint64_t pl_rtm1;   ///< System time of request.
+  uint64_t pl_mtm2;   ///< Steady time of response.
+  uint64_t pl_rtm2;   ///< System time of response.
+} payload;
+
+#endif

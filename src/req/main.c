d478a9bc135d8720920fa647a51330e9ff43f2e3
Daniel Lovasko <daniel.lovasko@gmail.com>
2018-08-07 09:17:53 -0400

    Threading work-in-progress.

--- src/req/main.c
+++ src/req/main.c
@@ -20,6 +20,8 @@
 #include <signal.h>
 #include <string.h>
 #include <errno.h>
+#include <pthread.h>
+#include <inttypes.h>
 
 #include "common/version.h"
 #include "common/payload.h"
@@ -33,9 +35,9 @@
 #define DEF_INTERVAL            1000000000 // One second pause between payloads.
 #define DEF_TIME_TO_LIVE        32         // IP Time-To-Live value.
 #define DEF_EXIT_ON_ERROR       false      // Process exit on publishing error.
-#define DEF_LOG_LEVEL           1          // Log errors and warnings by default.
+#define DEF_LOG_LEVEL           LL_DEBUG    // Log errors and warnings by default.
 #define DEF_LOG_COLOR           true       // Colors in the notification output.
-#define DEF_UDP_PORT            23000      // UDP 
+#define DEF_UDP_PORT            23000      // UDP port number to use
 #define DEF_RECEIVE_BUFFER_SIZE 2000000    // Socket receive buffer memory size.
 #define DEF_SEND_BUFFER_SIZE    2000000    // Socket send buffer memory size.
 
@@ -45,13 +47,13 @@
 #define NEMO_TARGET_TYPE_NAME 3 ///< Named address.
 
 // Target limits.
-#define NEMO_TARGET_NAME_MAX  64
+#define NEMO_TARGET_NAME_MAX  63
 #define NEMO_TARGET_COUNT_MAX 512
 
 /// Network endpoint target.
 typedef struct _target {
-  uint8_t  tg_type;                        ///< Type of the target (one of NEMO_TARGET_*).
-  char     tg_name[NEMO_TARGET_COUNT_MAX]; ///< Name used to describe 
+  uint8_t  tg_type;                        ///< One of NEMO_TARGET_TYPE*.
+  char     tg_name[NEMO_TARGET_COUNT_MAX]; ///< Name service identifier. 
   uint64_t tg_laddr;                       ///< Low bits of the address.
   uint64_t tg_haddr;                       ///< High bits of the address.
   uint64_t tg_time;                        ///< Time of last name resolution.
@@ -75,8 +77,48 @@ static bool     op_ipv6; ///< IPv6-only mode.
 static int sock4;                         ///< IPv4 socket.
 static int sock6;                         ///< IPv6 socket.
 static bool sint;                         ///< SIGINT flag.
+static bool susr1;                        ///< SIGUSR1 flag.
 static uint64_t ntgs;                     ///< Number of targets.
 static target tgs[NEMO_TARGET_COUNT_MAX]; ///< Target database.
+static pthread_t sender4;                 ///< IPv4 sender thread.
+static pthread_t recver4;                 ///< IPv4 receiver thread.
+static pthread_t sender6;                 ///< IPv6 sender thread.
+static pthread_t recver6;                 ///< IPv6 receiver thread.
+
+
+/// Send the SIGUSR1 signal to all other worker threads apart from the calling
+/// one.
+static void
+kill_other_threads(void)
+{
+  pthread_t self;
+  int reti;
+
+  // Rationale: it is expected that the thread that calls this function will
+  // exit on its own and therefore the SIGUSR1 signal should be only sent to
+  // the rest of the operating threads.
+  self = pthread_self();
+
+  if (op_ipv4 == true) {
+    reti = pthread_equal(self, sender4);
+    if (reti == 0)
+      (void)pthread_kill(sender4, SIGUSR1);
+
+    reti = pthread_equal(self, recver4);
+    if (reti == 0)
+      (void)pthread_kill(recver4, SIGUSR1);
+  }
+
+  if (op_ipv6 == true) {
+    reti = pthread_equal(self, sender6);
+    if (reti == 0)
+      (void)pthread_kill(sender6, SIGUSR1);
+
+    reti = pthread_equal(self, recver6);
+    if (reti == 0)
+      (void)pthread_kill(recver6, SIGUSR1);
+  }
+}
 
 static void
 print_usage(void)
@@ -102,8 +144,8 @@ print_usage(void)
     "  -i DUR  Interval duration between published datagram rounds. (def=1s)\n"
     "  -k KEY  Key for the current run. (def=random)\n"
     "  -n      Turn off colors in logging messages.\n"
-    "  -r RBS  Send buffer size in bytes.\n"
-    "  -s SBS  Send buffer size in bytes.\n"
+    "  -r RBS  Receive memory buffer size.\n"
+    "  -s SBS  Send memory buffer size.\n"
     "  -p NUM  UDP port to use for all endpoints. (def=%d)\n"
     "  -t TTL  Set the Time-To-Live for all published datagrams. (def=%d)\n"
     "  -v      Increase the verbosity of the logging output.\n",
@@ -140,13 +182,14 @@ generate_key(void)
   return key;
 }
 
-/// Parse the command-line arguments.
+/// Parse the command-line options.
 /// @return status code
 ///
-/// @param[in] argc argument count
-/// @param[in] argv argument vector
+/// @param[out] pidx  index where positional arguments start
+/// @param[in]  argc argument count
+/// @param[in]  argv argument vector
 static bool
-parse_arguments(int argc, char* argv[])
+parse_options(int* pidx, int argc, char* argv[])
 {
   int opt;
 
@@ -165,7 +208,7 @@ parse_arguments(int argc, char* argv[])
   op_ipv6 = false;
 
   // Loop through available options.
-  while ((opt = getopt(argc, argv, "46c:ehk:np:r:s:t:v")) != -1) {
+  while ((opt = getopt(argc, argv, "46c:ehi:k:np:r:s:t:v")) != -1) {
     switch (opt) {
 
       // IPv4-only mode.
@@ -192,8 +235,14 @@ parse_arguments(int argc, char* argv[])
       // Usage information.
       case 'h':
         print_usage();
-	_exit(EXIT_FAILURE);
+        _exit(EXIT_FAILURE);
         return false;
+
+      // Time interval between rounds of sent datagrams.
+      case 'i':
+        if (parse_scalar(&op_int, optarg, "ns", parse_time_unit) == false)
+          return false;
+        break;
 
       // Key of the current run.
       case 'k':
@@ -203,33 +252,33 @@ parse_arguments(int argc, char* argv[])
 
       // UDP port to use.
       case 'p':
-	if (parse_uint64(&op_port, optarg, 1, 65535) == false)
-	  return false;
-	break;
+        if (parse_uint64(&op_port, optarg, 1, 65535) == false)
+          return false;
+        break;
 
       // Socket receive buffer size.
       case 'r':
-	if (parse_scalar(&op_rbuf, optarg, "b", parse_memory_unit) == false)
-	  return false;
-	break;
+        if (parse_scalar(&op_rbuf, optarg, "b", parse_memory_unit) == false)
+          return false;
+        break;
 
       // Socket send buffer size.
       case 's':
-	if (parse_scalar(&op_sbuf, optarg, "b", parse_memory_unit) == false)
-	  return false;
-	break;
+        if (parse_scalar(&op_sbuf, optarg, "b", parse_memory_unit) == false)
+          return false;
+        break;
 
       // Set the outgoing Time-To-Live value.
       case 't':
-	if (parse_unit64(&op_ttl, optarg, 1, 255) == false)
-	  return false;
-	break;
+        if (parse_uint64(&op_ttl, optarg, 1, 255) == false)
+          return false;
+        break;
 
       // Increase the logging verbosity.
       case 'v':
-	if (op_llvl != LL_DEBUG)
-	  op_llvl++;
-	break;
+        if (op_llvl != LL_DEBUG)
+          op_llvl++;
+        break;
 
       // Unknown option.
       case '?':
@@ -255,6 +304,8 @@ parse_arguments(int argc, char* argv[])
     op_ipv4 = true;
     op_ipv6 = true;
   }
+
+	*pidx = optind;
 
   return true;
 }
@@ -287,6 +338,9 @@ parse_targets(int idx, int argc, char* argv[])
   struct in_addr addr4;
   struct in6_addr addr6;
   uint64_t tidx;
+	char str[64];
+
+	memset(str, '\0', sizeof(str));
   
   tidx = 0;
   for (i = idx; i < argc; i++) {
@@ -296,18 +350,24 @@ parse_targets(int idx, int argc, char* argv[])
     // Try parsing the address as IPv4.
     reti = inet_pton(AF_INET, argv[i], &addr4); 
     if (reti == 1) {
-      tgs[tidx].tg_type  = NEMO_TARGET_IPV4;
+      tgs[tidx].tg_type  = NEMO_TARGET_TYPE_IPV4;
       tgs[tidx].tg_laddr = (uint64_t)addr4.s_addr;
       tgs[tidx].tg_haddr = (uint64_t)0;
+
+			log_(LL_INFO, false, "parsed %s target: %s", "IPv4", argv[i]);
+      ntgs++;
       continue;
     }
     
     // Try parsing the address as IPv6.
     reti = inet_pton(AF_INET6, argv[i], &addr6);
     if (reti == 1) {
-      tgs[tidx].tg_type  = NEMO_TARGET_IPV6;
+      tgs[tidx].tg_type  = NEMO_TARGET_TYPE_IPV6;
       tgs[tidx].tg_laddr = make_address_part(&addr6.s6_addr[0]);
       tgs[tidx].tg_haddr = make_address_part(&addr6.s6_addr[7]);
+
+			log_(LL_INFO, false, "parsed %s target: %s", "IPv6", argv[i]);
+      ntgs++;
       continue;
     }
 
@@ -333,6 +393,9 @@ signal_handler(int sig)
 {
   if (sig == SIGINT)
     sint = true;
+
+  if (sig == SIGUSR1)
+		susr1 = true;
 }
 
 /// Install signal handlers.
@@ -342,18 +405,35 @@ install_signal_handler(void)
 {
   struct sigaction sa;
   int reti;
+  sigset_t sset;
 
   // Reset the signal indicator.
   sint = false;
+  susr1 = false;
+
+  // Create a set of blocked signals.
+  (void)sigfillset(&sset);
+  (void)sigdelset(&sset, SIGINT);
+  (void)sigdelset(&sset, SIGUSR1);
+  (void)sigprocmask(SIG_SETMASK, &sset, NULL);
 
   // Initialise the handler settings.
   (void)memset(&sa, 0, sizeof(sa));
   sa.sa_handler = signal_handler;
 
   // Install signal handler for SIGINT.
+	log_(LL_INFO, false, "installing signal handler for %s", "SIGINT");
   reti = sigaction(SIGINT, &sa, NULL);
   if (reti == -1) {
-    log_(NL_WARN, true, "unable to add signal handler for %s", "SIGINT");
+    log_(LL_WARN, true, "unable to add signal handler for %s", "SIGINT");
+    return false;
+  }
+
+  // Install signal handler for SIGUSR1.
+	log_(LL_INFO, false, "installing signal handler for %s", "SIGUSR1");
+  reti = sigaction(SIGUSR1, &sa, NULL);
+  if (reti == -1) {
+    log_(LL_WARN, true, "unable to add signal handler for %s", "SIGUSR1");
     return false;
   }
 
@@ -505,7 +585,59 @@ create_socket6(void)
   return true;
 }
 
-static bool
+/// Convert the target address to a universal standard address type.
+///
+/// @param[out] ss universal address type
+/// @param[in]  tg network target
+static void
+set_target_address(struct sockaddr_storage* ss, const target tg)
+{
+  struct sockaddr_in sin;
+	struct sockaddr_in6 sin6;
+
+	if (tg.tg_type == NEMO_TARGET_TYPE_IPV4) {
+		sin.sin_family      = AF_INET;
+		sin.sin_port        = htons((uint16_t)op_port);
+		sin.sin_addr.s_addr = (uint32_t)tg.tg_laddr;
+    (void)memcpy(ss, &sin, sizeof(sin));
+	}
+
+	if (tg.tg_type == NEMO_TARGET_TYPE_IPV6) {
+		sin6.sin6_family = AF_INET6;
+		sin6.sin6_port   = htons((uint16_t)op_port);
+		tipv6(&sin6.sin6_addr, tg.tg_laddr, tg.tg_haddr);
+		(void)memcpy(ss, &sin6, sizeof(sin6));		
+	}
+}
+
+/// Fill the payload with default and selected data.
+///
+/// @param[out] pl   payload
+/// @param[in]  tg   network target
+/// @param[in]  snum sequence number
+/// @param[in]  rts  system time
+/// @param[in]  mts  steady time
+static void
+fill_payload(payload *pl,
+             const target* tg,
+             const uint64_t snum,
+             const struct timespec rts,
+             const struct timespec mts)
+{
+  (void)memset(pl, 0, sizeof(*pl));
+  pl->pl_mgic = NEMO_PAYLOAD_MAGIC;
+  pl->pl_fver = NEMO_PAYLOAD_VERSION;
+  pl->pl_type = NEMO_PAYLOAD_TYPE_REQUEST;
+  pl->pl_port = (uint16_t)op_port;
+  pl->pl_ttl1 = (uint8_t)op_ttl;
+  pl->pl_snum = snum;
+  pl->pl_slen = op_cnt;
+  pl->pl_reqk = op_key;
+  pl->pl_laddr = tg->tg_laddr;
+  pl->pl_haddr = tg->tg_haddr;
+  tnanos(&pl->pl_rtm1, rts);
+  tnanos(&pl->pl_mtm1, mts);
+}
 
 /// Send a request to all selected targets.
 /// @return status code
@@ -521,52 +653,31 @@ send_all_datagrams(int sock, const uint64_t snum)
   struct timespec mts;
   struct msghdr msg;
   struct iovec data;
-  int ret;
+  struct sockaddr_storage addr;
   ssize_t n;
 
-  // Populate payload fields.
-  (void)memset(&pl, 0, sizeof(pl));
-  pl.pl_mgic = NEMO_PAYLOAD_MAGIC;
-  pl.pl_fver = NEMO_PAYLOAD_VERSION;
-  pl.pl_type = NEMO_PAYLOAD_TYPE_REQUEST;
-  pl.pl_port = (uint16_t)op_port;
-  pl.pl_ttl1 = (uint8_t)op_ttl;
-  pl.pl_snum = snum;
-  pl.pl_slen = op_cnt;
-  pl.pl_reqk = op_key;
-
   // Prepare payload data.
-  data.iov_base = pl;
-  data.iov_len  = sizeof(*pl);
+  data.iov_base = &pl;
+  data.iov_len  = sizeof(pl);
 
   // Prepare the message.
-  msg.msg_name       = addr;
-  msg.msg_namelen    = sizeof(*addr);
+  msg.msg_name       = &addr;
+  msg.msg_namelen    = sizeof(addr);
   msg.msg_iov        = &data;
   msg.msg_iovlen     = 1;
   msg.msg_control    = NULL;
   msg.msg_controllen = 0;
 
   for (i = 0; i < ntgs; i++) {
-    // Obtain the current system time.
-    ret = clock_gettime(CLOCK_REALTIME, &rts);
-    if (ret == -1) {
-      log_(LL_WARN, true, "unable to obtain current system time");
-      return false;
-    }
-    tnanos(&pl.pl_rtm1, rts);
-     
-    // Obtain the current steady time.
-    ret = clock_gettime(CLOCK_MONOTONIC, &mts);
-    if (ret == -1) {
-      log_(LL_WARN, true, "unable to obtain current system time");
-      return false;
-    }
-    tnanos(&pl.pl_rtm1, mts);
+	  // Assigning the correct address used by the target. This has to be called
+		// before the encode_.
+    set_target_address(&addr, tgs[i]);
 
-    // Assign target address.
-    pl.pl_laddr = tgs[i].tg_laddr;
-    pl.pl_haddr = tgs[i].tg_haddr;
+    // Prepare payload.
+    (void)clock_gettime(CLOCK_REALTIME,  &rts);
+    (void)clock_gettime(CLOCK_MONOTONIC, &mts);
+    fill_payload(&pl, &tgs[i], snum, rts, mts);
+    encode_payload(&pl);
 
     // Send the datagram.
     n = sendmsg(sock, &msg, MSG_DONTWAIT);
@@ -585,6 +696,130 @@ send_all_datagrams(int sock, const uint64_t snum)
         return false;
     }
   }
+
+	return true;
+}
+
+/// Thread that sends requests to targets on a specified socket.
+/// @return NULL
+///
+/// @param[in] arg socket file descriptor
+static void*
+send_worker(void* arg)
+{
+  int sock;
+  uint64_t i;
+  int reti;
+	bool retb;
+  struct timespec dur;
+
+  // Resolve the argument to a socket.
+  sock = *(int*)arg;
+	log_(LL_INFO, false, "starting sender thread for %s",
+	  sock == sock4 ? "IPv4" : "IPv6");
+
+  // Convert the interval duration.
+  fnanos(&dur, op_int);
+
+  // Start the sending loop.
+  for (i = 0; i < op_cnt; i++) {
+
+    log_(LL_DEBUG, false, "sending datagram round %" PRIu64 "/%" PRIu64, 
+      i + 1, op_cnt);
+
+    // Send all datagrams.
+    retb = send_all_datagrams(sock, i);
+    if (retb == false) {
+      log_(LL_WARN, false, "unable to send all payloads to IPv4 socket");
+
+      if (op_err == true) {
+        kill_other_threads();
+        break;
+      }
+    }
+
+    // Sleep for the desired interval.
+    reti = nanosleep(&dur, NULL);
+    if (reti == -1) {
+
+      // Check if the sleep was interrupted by a signal.
+      if (errno == EINTR) {
+        if (sint == true) {
+          kill_other_threads();
+          break;
+        }
+
+        if (susr1 == true) {
+					log_(LL_INFO, false, "exitting this thread");
+          break;
+				}
+      }
+
+      log_(LL_WARN, true, "unable to sleep");
+    }
+  }
+
+  return NULL;
+}
+
+/// Thread that receives responses on a specified socket.
+/// @return NULL
+///
+/// @param[in] arg socket file descriptor
+static void*
+recv_worker(void* arg)
+{
+  int sock;
+  ssize_t retss;
+	payload pl;
+  struct msghdr msg;
+  struct iovec iov;
+	struct sockaddr_storage addr;
+
+  // Resolve the argument to a socket.
+  sock = *(int*)arg;
+	log_(LL_INFO, false, "starting receiver thread for %s",
+	  sock == sock4 ? "IPv4" : "IPv6");
+
+  // Prepare payload data.
+  iov.iov_base = &pl;
+  iov.iov_len  = sizeof(pl);
+
+  // Prepare the message.
+  msg.msg_name       = &addr;
+  msg.msg_namelen    = sizeof(addr);
+  msg.msg_iov        = &iov;
+  msg.msg_iovlen     = 1;
+  msg.msg_control    = NULL;
+  msg.msg_controllen = 0;
+
+  // Receive datagrams.
+  while (true) {
+    retss = recvmsg(sock, &msg, MSG_TRUNC);
+
+    if (retss == -1) {
+      // Check if we were interrupted by a signal delivery.
+      if (errno == EINTR) {
+        
+        // If the signal was SIGINT, send SIGUSR1 to all other worker threads
+        // to interrupt their system calls.
+        if (sint == true) {
+          kill_other_threads();
+          break;
+        }
+
+        // Perform orderly exit after receiving the SIGUSR1 signal.
+        if (susr1 == true)
+          break;
+      }
+
+      log_(LL_WARN, true, "unable to receive datagrams");
+    }
+
+    log_(LL_DEBUG, false, "received message, responder key = %" PRIu64 "!", pl.pl_resk);
+  }
+
+  return NULL;
 }
 
 /// Start emitting the network requests.
@@ -592,82 +827,75 @@ send_all_datagrams(int sock, const uint64_t snum)
 static bool
 request_loop(void)
 {
-  uint64_t i;
-  payload pl;
   int reti;
-  bool retb;
-  struct timespec st_ts;
-  fd_set rfd;
 
-  log_(LL_INFO, false, "starting the request loop");
+	log_(LL_INFO, false, "starting the request loop");
 
-  // Add sockets to the event list.
-  if (op_ipv4)
-    FD_SET(sock4, &rfd);
-  if (op_ipv6)
-    FD_SET(sock6, &rfd);
-
-  // Emit a selected number of requests.
-  for (i = 0; i < op_cnt; i++) {
-
-    // Send all IPv4 requests.
-    retb = send_all_datagrams(sock4, i);
-    if (!retb) {
-      log_(LL_WARN, false, "unable to send all requests");
-
-      // Quit the loop if network transmissions errors should be fatal.
-      if (op_err)
-        return false;
+  // Start the IPv4 sending/receiving.
+  if (op_ipv4 == true) {
+    // Create the IPv4 receiver thread.
+    reti = pthread_create(&recver4, NULL, recv_worker, &sock4);
+    if (reti != 0) {
     }
 
-    // Send all IPv6 requests.
-    retb = send_all_datagrams(sock6, i);
-    if (!retb) {
-      log_(LL_WARN, false, "unable to send all requests");
-
-      // Quit the loop if network transmissions errors should be fatal.
-      if (op_err)
-        return false;
-    }
-
-    // Save the start time.
-    reti = clock_gettime(CLOCK_MONOTONIC, &st_ts);
+    // Create the IPv4 sender thread.
+    reti = pthread_create(&sender4, NULL, send_worker, &sock4);
     if (reti == -1) {
-      log_(LL_WARN, true, "unable to get current steady time");
-      return false;
     }
-    tnanos(&st_ns, st_ts);
+  }
 
-    while (true) {
-      reti = clock_gettime(CLOCK_MONOTONIC);
-      if (reti == -1) {
-        log_(LL_WARN, true, "unable to get current steady time");
-	return false;
-      }
+  // Start the IPv6 sending/receiving.
+  if (op_ipv6 == true) {
+    // Create the IPv6 receiver thread.
+    reti = pthread_create(&recver6, NULL, recv_worker, &sock6);
+    if (reti == -1) {
+    }
 
-      reti = pselect(5, &rfd, NULL, NULL, rem_ts, NULL);
-      if (reti == 
+    // Create the IPv6 sender thread.
+    reti = pthread_create(&sender6, NULL, send_worker, &sock6);
+    if (reti == -1) {
+    }
+  }
+
+  // Wait for the delivery of the SIGINT signal. Ignore the delivery of any
+  // other signal. Since the delivery of a signal can be dispatched to any
+  // thread, we need to terminate all worker threads. Once we send the USR1
+  // signal to all of them to interrupt the system calls they are  in, we wait
+  // for their orderly finish by joining them to the main thread.
+
+  while (true) {
+    (void)pause();
+    
+    if (sint == true) {
+      kill_other_threads();
+      (void)pthread_join(sender4, NULL);
+      (void)pthread_join(recver4, NULL);
+      (void)pthread_join(sender6, NULL);
+      (void)pthread_join(recver6, NULL);
+      break;
     }
   }
 
   return true;
 }
 
+
 /// Unicast network requester.
 int
 main(int argc, char* argv[])
 {
   bool retb;
+	int pidx;
 
-  // Parse command-line arguments.
-  retb = parse_arguments(argc, argv);
+  // Parse command-line options.
+  retb = parse_options(&pidx, argc, argv);
   if (retb == false) {
-    log_(LL_ERROR, false, "unable to parse command-line arguments");
+    log_(LL_ERROR, false, "unable to parse command-line options");
     return EXIT_FAILURE;
   }
 
   // Parse network targets.
-  retb = parse_targets();
+  retb = parse_targets(pidx, argc, argv);
   if (retb == false) {
     log_(LL_ERROR, false, "unable to parse network targets");
     return EXIT_FAILURE;

9930a36a0bf732672529120f19e627b89399ba6c
Daniel Lovasko <daniel.lovasko@gmail.com>
2018-09-06 00:54:29 +0000

    Added missing legal header.

--- src/util/daemon.c
+++ src/util/daemon.c
@@ -1,3 +1,9 @@
+// Copyright (c) 2018 Daniel Lovasko.
+// All Rights Reserved
+//
+// Distributed under the terms of the 2-clause BSD License. The full
+// license is in the file LICENSE, distributed as part of this software.
+
 #include <sys/types.h>
 #include <sys/stat.h>
 

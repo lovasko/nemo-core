cee5f1e51beedb4ba8013e8e3e18de126afd5a76
Daniel Lovasko <daniel.lovasko@gmail.com>
2018-10-01 02:28:25 +0100

    Function prototypes header.

---
+++ src/res/proto.h
@@ -0,0 +1,40 @@
+// Copyright (c) 2018 Daniel Lovasko.
+// All Rights Reserved
+//
+// Distributed under the terms of the 2-clause BSD License. The full
+// license is in the file LICENSE, distributed as part of this software.
+
+#ifndef NEMO_RES_PROTO_H
+#define NEMO_RES_PROTO_H
+
+#include <sys/socket.h>
+
+#include <stdlib.h>
+
+#include "common/payload.h"
+#include "res/types.h"
+
+
+// Payload.
+bool verify_payload(const ssize_t n, const payload* pl);
+bool update_payload(payload* pl, struct msghdr* msg, const struct options* opts);
+
+// Report.
+void report_header(const struct options* opts);
+void report_event(const payload* pl, const struct options* opts);
+bool flush_report_stream(const struct options* opts);
+
+// Options.
+bool parse_options(struct options* opts, int argc, char* argv[]);
+void log_options(const struct options* opts);
+
+// Plugins.
+bool load_plugins(struct plugin* pins, uint64_t* npins, const struct options* opts);
+bool init_plugins(const struct plugin* pins, const uint64_t npins);
+void exec_plugins(const struct plugin* pins, const uint64_t npins, const payload* pl);
+void free_plugins(const struct plugin* pins, const uint64_t npins);
+
+// Event.
+bool handle_event(int sock, const char* ipv, const struct options* opts);
+
+#endif

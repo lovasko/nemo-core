0aae7fc650561ba53671cda50f32803aac6c7458
Daniel Lovasko <daniel.lovasko@gmail.com>
2018-10-23 07:52:00 -0400

    Separated signal handling into a separate unit.

---
+++ src/ureq/signal.c
@@ -0,0 +1,104 @@
+// Copyright (c) 2018 Daniel Lovasko.
+// All Rights Reserved
+//
+// Distributed under the terms of the 2-clause BSD License. The full
+// license is in the file LICENSE, distributed as part of this software.
+
+#include <stdbool.h>
+
+
+volatile bool sint;  ///< SIGINT flag.
+volatile bool sterm; ///< SIGTERM flag.
+
+/// Signal handler for the SIGINT and SIGTERM signals.
+///
+/// This handler does not perform any action, just toggles the indicator
+/// for the signal. The actual signal handling is done by the respond_loop
+/// function.
+///
+/// @global sint
+/// @global sterm
+///
+/// @param[in] sig signal number
+static void
+signal_handler(int sig)
+{
+  if (sig == SIGINT)  { sint  = true; }
+  if (sig == SIGTERM) { sterm = true; }
+}
+
+/// Block the delivery of all signals, except the two signals that can not be
+/// intercepted.
+static void
+block_all_signals(void)
+{
+  sigset_t mask;
+
+  // All signals that are not part of the set are standard signals and
+  // therefore the listed errors do not apply.
+  (void)sigfillset(&mask);
+  (void)sigdelset(&mask, SIGSTOP);
+  (void)sigdelset(&mask, SIGKILL);
+
+  // None of the two errno values apply to the call and therefore we can assume
+  // that the call always succeeds.
+  (void)sigprocmask(SIG_SETMASK, &mask, NULL);
+}
+
+/// Install signal handlers for the SIGINT and SIGTER signals.
+/// @return success/failure indication
+///
+/// @global sint
+/// @global sterm
+bool
+install_signal_handlers(void)
+{
+  struct sigaction sa;
+  int reti;
+
+  log(LL_INFO, false, "main", "installing signal handlers");
+
+  // Reset the signal indicator.
+  sint  = false;
+  sterm = false;
+
+  // This action makes sure that no system calls or execution context will get
+  // interrupted by a signal. The pselect(2) call explicitly enables the
+  // delivery of a set of signals, which in turn set the indicator flags.
+  block_all_signals();
+
+  // Initialise the handler settings.
+  (void)memset(&sa, 0, sizeof(sa));
+  sa.sa_handler = signal_handler;
+
+  // Install signal handler for SIGINT.
+  reti = sigaction(SIGINT, &sa, NULL);
+  if (reti == -1) {
+    log(LL_WARN, true, "main", "unable to add signal handler for %s",
+        "SIGINT");
+    return false;
+  }
+
+  // Install signal handler for SIGTERM.
+  reti = sigaction(SIGTERM, &sa, NULL);
+  if (reti == -1) {
+    log(LL_WARN, true, "main", "unable to add signal handler for %s",
+        "SIGTERM");
+    return false;
+  }
+
+  return true;
+}
+
+/// Create a signal mask that enables the SIGINT and SIGTERM signals.
+///
+/// @param[out] mask signal mask applied while waiting
+void
+create_signal_mask(sigset_t* mask)
+{
+  (void)sigfillset(mask);
+  (void)sigdelset(mask, SIGSTOP);
+  (void)sigdelset(mask, SIGKILL);
+  (void)sigdelset(mask, SIGINT);
+  (void)sigdelset(mask, SIGTERM);
+}

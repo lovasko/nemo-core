9ab7fdc6e3eff0aa31b56f2e552595438ac2493b
Daniel Lovasko <daniel.lovasko@gmail.com>
2018-11-28 16:48:29 +0000

    Recognized the USR1 signal.

--- src/ureq/signal.c
+++ src/ureq/signal.c
@@ -13,6 +13,7 @@
 
 volatile bool sint;  ///< SIGINT flag.
 volatile bool sterm; ///< SIGTERM flag.
+volatile bool susr1; ///< SIGUSR1 flag.
 
 /// Signal handler for the SIGINT and SIGTERM signals.
 ///
@@ -22,6 +23,7 @@ volatile bool sterm; ///< SIGTERM flag.
 ///
 /// @global sint
 /// @global sterm
+/// @global susr1
 ///
 /// @param[in] sig signal number
 static void
@@ -29,6 +31,7 @@ signal_handler(int sig)
 {
   if (sig == SIGINT)  { sint  = true; }
   if (sig == SIGTERM) { sterm = true; }
+  if (sig == SIGUSR1) { susr1 = true; }
 }
 
 /// Block the delivery of all signals, except the two signals that can not be
@@ -54,6 +57,7 @@ block_all_signals(void)
 ///
 /// @global sint
 /// @global sterm
+/// @global susr1
 bool
 install_signal_handlers(void)
 {
@@ -91,10 +95,18 @@ install_signal_handlers(void)
     return false;
   }
 
+  // Install signal handler for SIGUSR1.
+  reti = sigaction(SIGUSR1, &sa, NULL);
+  if (reti == -1) {
+    log(LL_WARN, true, "main", "unable to add signal handler for %s",
+        "SIGUSR1");
+    return false;
+  }
+
   return true;
 }
 
-/// Create a signal mask that enables the SIGINT and SIGTERM signals.
+/// Create a signal mask that enables the SIGINT, SIGTERM, and SIGUSR1 signals.
 ///
 /// @param[out] mask signal mask applied while waiting
 void
@@ -105,4 +117,5 @@ create_signal_mask(sigset_t* mask)
   (void)sigdelset(mask, SIGKILL);
   (void)sigdelset(mask, SIGINT);
   (void)sigdelset(mask, SIGTERM);
+  (void)sigdelset(mask, SIGUSR1);
 }

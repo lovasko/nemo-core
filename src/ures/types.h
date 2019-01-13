eb104f678dbb2080d8661f82e5209b37b13add9c
Daniel Lovasko <daniel.lovasko@gmail.com>
2019-01-13 21:44:44 +0000

    Clarified object name in comments.

--- src/ures/types.h
+++ src/ures/types.h
@@ -57,13 +57,13 @@ struct plugin {
 
 /// Internet protocol connection.
 struct proto {
-  uint64_t    pr_rall; ///< Number of overall received messages.
+  uint64_t    pr_rall; ///< Number of overall received datagrams.
   uint64_t    pr_reni; ///< Received errors due to network issues.
   uint64_t    pr_resz; ///< Received errors due to size mismatch.
   uint64_t    pr_remg; ///< Received errors due to magic number mismatch.
   uint64_t    pr_repv; ///< Received errors due to payload version mismatch.
   uint64_t    pr_rety; ///< Received errors due to payload type.
-  uint64_t    pr_sall; ///< Number of overall sent messages.
+  uint64_t    pr_sall; ///< Number of overall sent datagrams.
   uint64_t    pr_seni; ///< Sent errors due to network issues.
   const char* pr_name; ///< Human-readable name.
   int         pr_sock; ///< Network socket.
